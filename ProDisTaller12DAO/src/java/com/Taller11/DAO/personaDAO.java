a269d031d7c693061cae126b449cadf5efa5ba14
miguelcamargo9 <miguelcamargo9@gmail.com>
2014-05-13 21:04:56 +0000

    Taller 12 Persistiendo con DAO y mostrando lista

---
+++ ProDisTaller12DAO/src/java/com/Taller11/DAO/personaDAO.java
@@ -0,0 +1,233 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Taller11.DAO;
+
+/**
+ *
+ * @author Miguel
+ */
+import com.Taller11.vo.persona;
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class personaDAO {
+  // ----------------------------------------------------
+  // Constantes
+  // ----------------------------------------------------
+
+  // ----------------------------------------------------
+  // Consultas
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+  // Atributos
+  // ----------------------------------------------------
+  /**
+   * conexion con la base de datos
+   */
+  public Connection conexion;
+  /**
+   * nombre del usuario para conectarse a la base de datos.
+   */
+  private String usuario;
+  /**
+   * clave de conexión a la base de datos.
+   */
+  private String clave;
+  /**
+   * URL al cual se debe conectar para acceder a la base de datos.
+   */
+  private String cadenaConexion;
+
+  /**
+   * constructor de la clase. No inicializa ningun atributo.
+   */
+  public personaDAO() {
+    inicializar();
+  }
+
+  // -------------------------------------------------
+  // Métodos
+  // -------------------------------------------------
+  /**
+   * obtiene ls datos necesarios para establecer una conexion Los datos se obtienen a partir de un archivo properties.
+   *
+   * @param path ruta donde se encuentra el archivo properties.
+   */
+  public void inicializar() {
+    try {
+//			File arch = new File(ARCHIVO_CONEXION);
+//			Properties prop = new Properties();
+//			FileInputStream in = new FileInputStream(arch);
+//
+//			prop.load(in);
+//			in.close();
+
+//			cadenaConexion = prop
+//					.getProperty("url"); // El
+      // url,
+      // el
+      // usuario
+      // y
+      // passwd
+      // deben
+      // estar
+      // en
+      // un
+      // archivo
+      // de
+      // propiedades.
+      // url: "jdbc:oracle:thin:@chie.uniandes.edu.co:1521:chie10";
+      cadenaConexion = "jdbc:mysql://localhost:3306/taller";
+//			usuario = prop.getProperty("usuario"); // "s2501aXX";
+      usuario = "root"; // "s2501aXX";
+//			clave = prop.getProperty("clave"); // "c2501XX";
+      clave = "miguelcamargo9"; // "c2501XX";
+//			final String driver = prop
+//					.getProperty("driver");
+      final String driver = "com.mysql.jdbc.Driver";
+
+      Class.forName(driver);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Método que se encarga de crear la conexión con el Driver Manager a partir de los parametros recibidos.
+   *
+   * @param url direccion url de la base de datos a la cual se desea conectar
+   * @param usuario nombre del usuario que se va a conectar a la base de datos
+   * @param clave clave de acceso a la base de datos
+   * @throws SQLException si ocurre un error generando la conexión con la base de datos.
+   */
+  private void establecerConexion(String url, String usuario, String clave)
+          throws SQLException {
+    try {
+      System.out.println("url " + url + " usr " + usuario + " pwd " + clave);
+      conexion = DriverManager.getConnection(url, usuario, clave);
+    } catch (SQLException exception) {
+      throw new SQLException(
+              "ERROR: personaDAO obteniendo una conexi—n.");
+    }
+  }
+
+  /**
+   * Cierra la conexión activa a la base de datos. Además, con=null.
+   *
+   * @param con objeto de conexión a la base de datos
+   * @throws SistemaCinesException Si se presentan errores de conexión
+   */
+  public void closeConnection(Connection connection) throws Exception {
+    try {
+      connection.close();
+      connection = null;
+    } catch (SQLException exception) {
+      throw new Exception(
+              "ERROR: personaDAO: closeConnection() = cerrando una conexión.");
+    }
+  }
+
+  // ---------------------------------------------------
+  // Métodos asociados a los casos de uso: Consulta
+  // ---------------------------------------------------
+    /*
+   * metodo que inserta una serie nueva
+   */
+  public void insertarPersona(persona miPersona) throws Exception {
+    try {
+      establecerConexion(cadenaConexion, usuario, clave);
+      PreparedStatement insercion = conexion.prepareStatement(
+              "insert into persona (nombre,apellido,documento) values(?,?,?)");
+      insercion.setString(1, miPersona.getNombre());
+      insercion.setString(2, miPersona.getApellido());
+      insercion.setInt(3, miPersona.getDocumento());
+      insercion.executeUpdate();
+    } catch (SQLException ex) {
+      Logger.getLogger(personaDAO.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      if (conexion != null) {
+        try {
+          closeConnection(conexion);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  /*
+   * metodo para eliminar persona
+   */
+  public void eliminarPersona(String nombre) throws Exception {
+    try {
+      establecerConexion(cadenaConexion, usuario, clave);
+      Statement insercion = conexion.createStatement();
+      String sql = "delete from persona where persona.nombre like '%" + nombre + "%'";
+
+      int delete = insercion.executeUpdate(sql);
+
+      if (delete == 1) {
+        System.out.println("fila borrada");
+      } else {
+        System.out.println("fila no borrada");
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(personaDAO.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      if (conexion != null) {
+        try {
+          closeConnection(conexion);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  /*
+   * metodo para traer personas
+   */
+  public List<persona> traerPersonas() throws Exception {
+    List<persona> misPersonas = new ArrayList<persona>();
+    try {
+      establecerConexion(cadenaConexion, usuario, clave);
+
+      ResultSet resultados = conexion.createStatement().
+              executeQuery("select nombre, apellido, documento from taller.persona ");
+
+      while (resultados.next()) {
+        String nombre = resultados.getString(1);
+        String apellido = resultados.getString(2);
+        int documento = resultados.getInt(3);
+        persona miPersona = new persona(nombre, apellido, documento);
+        misPersonas.add(miPersona);
+      }
+
+
+    } catch (SQLException ex) {
+      Logger.getLogger(personaDAO.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      if (conexion != null) {
+        try {
+          closeConnection(conexion);
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return misPersonas;
+  }
+}
---
+++ ProDisTaller12DAO/src/java/com/Taller11/servlets/personaServlet.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Taller11.servlets;
+
+import com.Taller11.DAO.personaDAO;
+import com.Taller11.vo.persona;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Miguel
+ */
+@WebServlet(name = "personaServlet", urlPatterns = {"/personaServlet"})
+public class personaServlet extends HttpServlet {
+  
+  private persona miPersona = new persona(null, null, Integer.MIN_VALUE);
+  private personaDAO miDao = new personaDAO();
+  private boolean bandera = true;
+  private List<persona> misPersonas = new ArrayList<persona>();
+
+  /**
+   * Processes requests for both HTTP
+   * <code>GET</code> and
+   * <code>POST</code> methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+      out.println("<head>");
+      out.println("<title>Servlet personaServlet</title>");      
+      out.println("</head>");
+      out.println("<body>");
+      if(bandera == true){
+        out.println("<h1>Guardado con Exito</h1>");
+      }else{
+        out.println("<h1>Error al Guardar</h1>");
+      }
+      out.println("<table>");
+      out.println("<tr>");
+      out.println("<th>Nombre");
+      out.println("</th>");
+      out.println("<th>Apellido");
+      out.println("</th>");
+      out.println("<th>Documento");
+      out.println("</th>");
+      out.println("</tr>");
+      for (persona miPersona : misPersonas) {
+        out.println("<tr>");
+        out.println("<td>"+miPersona.getNombre());
+        out.println("</td>");
+        out.println("<td>"+miPersona.getApellido());
+        out.println("</td>");
+        out.println("<td>"+miPersona.getDocumento());
+        out.println("</td>");
+        out.println("</tr>");
+      }
+      out.println("</table>");
+      out.println("<a href='index.jsp' >Volver</a>");
+      out.println("</body>");
+      out.println("</html>");
+    } finally {      
+      out.close();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP
+   * <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP
+   * <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    String nombre = request.getParameter("nombre");
+    String apellido = request.getParameter("apellido");
+    Integer documento = Integer.parseInt(request.getParameter("documento"));
+    miPersona.setNombre(nombre);
+    miPersona.setApellido(apellido);
+    miPersona.setDocumento(documento);
+    try {
+      miDao.insertarPersona(miPersona);
+      misPersonas = miDao.traerPersonas();
+      bandera = true;
+    } catch (Exception ex) {
+      bandera = false;
+      Logger.getLogger(personaServlet.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+}
