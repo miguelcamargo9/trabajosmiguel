d7eaa9f2ee3466e8a72a31ead682a1a5127032a8
miguelcamargo9 <miguelcamargo9@gmail.com>
2014-05-13 22:21:53 +0000

    Ultimmos Ajustes Calculadora JSF

--- CalculadoraJSF/src/java/com/calculadoraJSF/managedBeans/calculadoraMB.java
+++ CalculadoraJSF/src/java/com/calculadoraJSF/managedBeans/calculadoraMB.java
@@ -6,13 +6,14 @@ package com.calculadoraJSF.managedBeans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author miguelcamargo9
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class calculadoraMB {
 
   /**
@@ -21,9 +22,8 @@ public class calculadoraMB {
   private double numero;
   private double resultado;
   private String operacion;
-  
-  public calculadoraMB() {
 
+  public calculadoraMB() {
   }
 
   public double getNumero() {
@@ -49,6 +49,53 @@ public class calculadoraMB {
   public void setOperacion(String operacion) {
     this.operacion = operacion;
   }
-  
-  
+
+  public void calcular() {
+    int Operac = 0;
+    if (this.operacion.equals("SUMAR")) {
+      Operac = 1;
+    }
+    if (this.operacion.equals("RESTAR")) {
+      Operac = 2;
+    }
+    if (this.operacion.equals("MULTIPLICAR")) {
+      Operac = 3;
+    }
+    if (this.operacion.equals("DIVIDIR")) {
+      Operac = 4;
+    }
+    if (this.operacion.equals("SIN")) {
+      Operac = 5;
+    }
+    if (this.operacion.equals("COS")) {
+      Operac = 6;
+    }
+    if (this.operacion.equals("TAN")) {
+      Operac = 7;
+    }
+
+    switch (Operac) {
+      case 1:
+        this.resultado = this.resultado + this.numero;
+        break;
+      case 2:
+        this.resultado = this.resultado - this.numero;
+        break;
+      case 3:
+        this.resultado = this.resultado * this.numero;
+        break;
+      case 4:
+        this.resultado = this.resultado / this.numero;
+        break;
+      case 5:
+        this.resultado = Math.sin(this.numero);
+        break;
+      case 6:
+        this.resultado = Math.cos(this.numero);
+        break;
+      case 7:
+        this.resultado = Math.tan(this.numero);
+        break;
+    }
+  }
 }
